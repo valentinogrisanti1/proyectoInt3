1704e41a28145ebb6161e536b573a8eb65b4bf0b
Luisa <luisa@MacBook-Pro-de-Luisa.local>
2021-09-13 09:34:59 -0300

    mm

--- proyecto-int-3/src/Components/Header/Header.js
+++ proyecto-int-3/src/Components/Header/Header.js
@@ -3,18 +3,22 @@ import './Header.css'
 
 function Header (){
     return(
+        
         <header >
-                <h1>PelisNet</h1>
-                <section className="barra-superior">
+               
+               <h4>PelisNet</h4> 
+                
+                <section>
                     <p>Ordenar ASC/ DESC</p>
                     <i className="fas fa-th"></i>
                     <i className="fas fa-align-justify"></i>
                     <form action="">
-                        <input className="barraBuscar" type="text" name="search" id="" placeholder="Search"/>
-                            <button id="boton" type="submit"><i className="fas fa-search"/></button>
+                        <input type="text" name="search" id="" placeholder="Search"/>
+                            <button type="submit"><i className="fas fa-search"/>enviar</button>
                     </form>
                 </section>
-        </header> 
+        </header>
+        
     );
 }
 
